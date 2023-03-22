0102139e0015bc2082888e169d5ed27f949ee487
Andrew <armartin1998@gmail.com>
2023-03-21 21:25:52 -0400

    Complete algorithms palindrome

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const palindromeCheck = word.split('').reverse().join('')
+  if (word === palindromeCheck){
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  if word === word backwards
+    return true
+  anything  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  isPlaindrome takes in a word and checks if it is spelled the
+  same backwards as it is forwards
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +32,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log(isPalindrome('mom'))
+  console.log(isPalindrome('wow'))
+  console.log(isPalindrome('Andy'))
 }
 
 module.exports = isPalindrome;
